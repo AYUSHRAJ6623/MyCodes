949e253fd410d5bb318c71dcf6a82e393f813490
Ayush Raj <ayushraj4169@gmail.com>
2023-04-25 15:19:29 +0530

    25 April 2023

--- Array_pointer.cpp
+++ Array_pointer.cpp
@@ -4,8 +4,19 @@ int main()
 {
     int arr[] = {1, 2, 3, 4, 5};
     int n = sizeof(arr) / sizeof(int);
-    cout << arr << endl;
     int *p = arr;
-    cout << p<< endl;
+    //  This 2 line print addres of arr.
+    cout << arr << endl;
+    //  cout<<p<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        // This will print value at *p(Element of Arr)
+        cout << *p++ << endl;
+    }
+    cout << endl;
+    for(int i=0;i<n;i++){
+        //This will print value of Arr 
+        cout<<*(arr+i)<<" ";
+    }
     return 0;
 }
