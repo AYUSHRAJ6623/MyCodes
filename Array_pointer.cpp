6a4ada4b190bb84c15b3489873dca49ed472c214
Ayush Raj <ayushraj4169@gmail.com>
2023-04-26 09:43:36 +0530

    25 April

--- Array_pointer.cpp
+++ Array_pointer.cpp
@@ -5,7 +5,7 @@ int main()
     int arr[] = {1, 2, 3, 4, 5};
     int n = sizeof(arr) / sizeof(int);
     int *p = arr;
-    //  This 2 line print addres of arr.
+    //  This 2 line print addres of arr
     cout << arr << endl;
     //  cout<<p<<endl;
     for (int i = 0; i < n; i++)
